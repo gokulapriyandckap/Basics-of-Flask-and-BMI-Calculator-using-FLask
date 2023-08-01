c53fb8311c07c7318dd171417a73ae05d06a32c3
gokulapriyandckap <bgokulapriyandckap@gmail.com>
2023-08-01 16:30:23 +0530

    # Handling the HTTP request , POST and GET Methods and also Practiced this in Postman.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -13,7 +12,12 @@ def welcome():
 def greetings():
     return 'Welcome To my First Flask app ;)'
 
-
-
+# Handling the HTTP request , POST and GET Methods and also Practiced this in Postman.
+@app.route('/method', methods=['GET','POST'])
+def method():
+    if request.method == 'POST':
+        return "You've used the Post Method!"
+    else:
+        return "You've Used the Get Method!"
 
 app.run()
